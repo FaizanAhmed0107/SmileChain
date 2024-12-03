4e81f8b9486b2fb315427e08560c6867cc0ca945
faizan <faizanahmed0107@gmail.com>
2024-12-04 00:19:27 +0530

    Improved Vercel integration

--- client/src/API_Requests/serverAddress.js
+++ client/src/API_Requests/serverAddress.js
@@ -1,5 +1,6 @@
 function serverAddress() {
-    return "http://localhost:5000"
+    // return "http://localhost:5000";
+    return "https://smile-chain-server.vercel.app";
 }
 
-export default serverAddress()
+export default serverAddress();
