18eb15c08b3a4ad152b57d07c22deee98b9a17e3
faiza <faizanahmed0107@gmail.com>
2025-01-06 19:33:12 +0530

    Added Session storage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,47 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-
 import TopBar from "./TopBar/TopBar.jsx";
 import WebCamCont from "./WebCamContain/WebCamCont.jsx";
 import PhotoFeed from "./PhotoFeed/PhotoFeed.jsx";
 import RegisterLogin from "./Login_Register/RegisterLogin.jsx";
 import DeviceDetector from "./DeviceDetector.jsx";
 
-
 function App() {
+    // Initialize state with values from sessionStorage (if they exist)
     const [isSmall, setIsSmall] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [AccessToken, setAccessToken] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(JSON.parse(sessionStorage.getItem("isLoggedIn")) || false);
+    const [AccessToken, setAccessToken] = useState(sessionStorage.getItem("AccessToken") || "");
     const [showLogin, setShowLogin] = useState(false);
+
+    // Sync state changes to sessionStorage
+    useEffect(() => {
+        sessionStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+    }, [isLoggedIn]);
+
+    useEffect(() => {
+        sessionStorage.setItem("AccessToken", AccessToken);
+    }, [AccessToken]);
 
     return (
         <>
             {showLogin ?
-                <RegisterLogin setIsLoggedIn={setIsLoggedIn} setAccessToken={setAccessToken}
-                               setShowLogin={setShowLogin} isSmall={isSmall}/>
-                : <>
-                    <TopBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setShowLogin={setShowLogin}
-                            AccessToken={AccessToken} setAccessToken={setAccessToken} isSmall={isSmall}/>
+                <RegisterLogin
+                    setIsLoggedIn={setIsLoggedIn}
+                    setAccessToken={setAccessToken}
+                    setShowLogin={setShowLogin}
+                    isSmall={isSmall}
+                />
+                :
+                <>
+                    <TopBar
+                        isLoggedIn={isLoggedIn}
+                        setIsLoggedIn={setIsLoggedIn}
+                        setShowLogin={setShowLogin}
+                        AccessToken={AccessToken}
+                        setAccessToken={setAccessToken}
+                        isSmall={isSmall}
+                    />
                     <WebCamCont isLoggedIn={isLoggedIn}/>
                     <PhotoFeed AccessToken={AccessToken} isLoggedIn={isLoggedIn}/>
                 </>
@@ -31,9 +50,7 @@ function App() {
             <DeviceDetector setIsSmall={setIsSmall}/>
             <ToastContainer/>
         </>
-    )
+    );
 }
 
 export default App;
-
-
