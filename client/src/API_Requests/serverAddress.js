9177489b49c69a411e10a6142f593826bca93303
faiza <faizanahmed0107@gmail.com>
2025-01-06 17:13:19 +0530

    updated cors properties

--- client/src/API_Requests/serverAddress.js
+++ client/src/API_Requests/serverAddress.js
@@ -1,6 +1,6 @@
 function serverAddress() {
-    return "http://localhost:5000";
-    // return "https://smile-chain-server.vercel.app";
+    // return "http://localhost:5000";
+    return "https://smile-chain-server.vercel.app";
 }
 
 export default serverAddress();
--- server/server.js
+++ server/server.js
@@ -21,13 +21,19 @@ const io = new Server(server, {
     cors: {
         origin: allowedOrigins,
         methods: ["GET", "POST"],
+        credentials: true, // Enable credentials if needed
     },
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.use(compression());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/api/image', require('./routes/imageRoute'));
 app.use('/api/users', require('./routes/userRoutes'));
